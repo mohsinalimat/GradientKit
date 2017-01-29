4fa48d206371a1631e8d5b83e2ee38a0785fa96e
Aashish Dhawan <aashishdhawan@gmail.com>
2017-01-29 22:10:42 +0530

    Added animation gif

--- Example/GradientKit/ViewController.swift
+++ Example/GradientKit/ViewController.swift
@@ -20,6 +20,22 @@ class ViewController: UIViewController {
         button.layer.borderWidth = 2.0
         button.layer.cornerRadius = 2
         button.isHidden = true
+        
+        
+        let layer  = AngleGradientLayer()
+        layer.colors = Colors.randomColors(numberOfColors: 5)
+        
+        let width = view.bounds.size.width
+        let height = view.bounds.size.height
+        
+        let diameter = max(width, height) + 100
+        let frame = CGRect(x: -55 - (height - width) / 2, y: -50, width: diameter, height: diameter)
+        layer.frame = frame
+        
+        layer.rotate()
+        view.layer.addSublayer(layer)
+        
+        
     }
 }
 
